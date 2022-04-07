eaf78a9c52300202bd09c2667d0711df076d9baa
azad1993 <azad.shahbazov@mail.ru>
2022-04-07 15:19:14 +0400

    changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    `~/assets/main.scss`
+    `@/assets/main.scss`
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
